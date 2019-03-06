48bf1588c185f783385006829896e93810153d0b
JeeseLee <546698606@qq.com>
2019-03-06 19:17:36 +0000

    Update Order.csv, CsvReader and Order class

--- OrderShop/src/shop/CsvReader.java
+++ OrderShop/src/shop/CsvReader.java
@@ -6,58 +6,129 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.TreeSet;
 
-public class CsvReader {
-	
-	CsvReader(){
-		
-		
-	}
-	
-	public ArrayList <Order> readOrdersInfo(String filename) throws FileNotFoundException {
-		ArrayList <Order> orderList = new ArrayList<Order>();
-		File file2 = new File(filename);
-		Scanner sc2 = new Scanner(file2);
+import ourExceptions.InvalidItemIDLengthException;
+import ourExceptions.InvalidOrderCustomerID;
+import ourExceptions.InvalidOrderTimeStamp;
+import ourExceptions.InvalidPriceException;
+import ourExceptions.InvalidCategoryException;
+import ourExceptions.InvalidItemException;
 
+// Here we have the readers that read the .csv files
+public class CsvReader {
+	private ArrayList<Order> orderList = new ArrayList<Order>();
+	private TreeSet<Item> itemList = new TreeSet<Item>(new ItemComparator());
+
+	public CsvReader() {
+	}
+
+	public TreeSet<Item> readMenuInfo(String filename) throws InvalidItemIDLengthException, FileNotFoundException,
+			InvalidItemIDLengthException, InvalidItemException, InvalidPriceException, InvalidCategoryException {
+		File file1 = new File(filename);
+		@SuppressWarnings("resource")
+		Scanner sc1 = new Scanner(file1);
+		// we don't need a Map since we don't have a key for the menu, we don't need a
+		// set since we don't want to get rid of the duplicates
+
+		String line = sc1.nextLine();
+		try {
+			while (sc1.hasNextLine()) {
+				line = sc1.nextLine();
+				String[] values = line.split(",");
+				String name = values[0];
+				String description = values[1];
+				double price = Double.parseDouble(values[2]);
+				if (price < 0 || price > 100) {
+					throw new InvalidPriceException();
+				}
+				String itemID = values[3];
+				if (itemID.length() != 8) {
+					throw new InvalidItemIDLengthException();
+				}
+				String category = values[4];
+				if (!(category.equals("Food")) && !(category.equals("Beverage")) && !(category.equals("Dessert"))) {
+					throw new InvalidCategoryException();
+				}
+				Item menuItem = new Item(name, description, price, itemID, category);
+				itemList.add(menuItem);
+
+			}
+		} catch (NumberFormatException nfe) {
+			System.err.print("Wrong format of price! Line '" + line + "' skipped.\n");
+			while (sc1.hasNext())
+				sc1.nextLine();
+		} catch (InvalidItemIDLengthException invalidItemIDLength) {
+			System.err.print("Invalid item ID's length in line: '" + line + "' !\n");
+			while (sc1.hasNext())
+				sc1.nextLine();
+		} catch (InvalidPriceException invalidPrice) {
+			System.err.print("Price should be between 0 and 100! Error found in line: '" + line + "' .\n");
+			while (sc1.hasNext())
+				sc1.nextLine();
+		} catch (InvalidCategoryException invalidCategory) {
+			System.err.print("Category is invalid!\n");
+			while (sc1.hasNext())
+				sc1.nextLine();
+		}
+		sc1.close();
+
+		return itemList;
+	}
+
+	public ArrayList<Order> readOrdersInfo(String filename)
+			throws FileNotFoundException, InvalidOrderTimeStamp, InvalidOrderCustomerID {
+
+		File file2 = new File(filename);
+		@SuppressWarnings("resource")
+		Scanner sc2 = new Scanner(file2);
 		String line = sc2.nextLine();
-		while (sc2.hasNextLine()) {
-			line = sc2.nextLine();
-			String[] values = line.split(",");
-			String timestamp = values[0];
-			String customerID = values[1];
-			String itemOrdered = values[2];
-			Order OrderedItem = new Order(timestamp, customerID, itemOrdered);
-			for (Order ord: orderList)
-				if(ord.getCustomerID() == customerID && ord.getTimeStamp() == timestamp)
-					ord.addItem(itemOrdered, 1);
-			orderList.add(OrderedItem);
+		try {
+			while (sc2.hasNextLine()) {
+				line = sc2.nextLine();
+				String[] values = line.split(",");
+				String timestamp = values[0];
+				if (timestamp.length() != 10) {
+					throw new InvalidOrderTimeStamp();
+				}
+				String customerID = values[1];
+				if (!(customerID.startsWith("CUST"))) {
+					throw new InvalidOrderCustomerID();
+				}
+				String customerName = values[2];
+				Item itemOrdered = findItem(values[3]);
+				Order newOrder = new Order(timestamp, customerID,customerName,itemOrdered);
+				boolean foundOrd = false;
+				for (Order ord : orderList) {
+					if (ord.getCustomerID().equals(customerID) && ord.getTimeStamp().equals(timestamp)) {
+						ord.addItem(itemOrdered, 1);
+						foundOrd = true;
+					}
+				}
+				if (!foundOrd)
+					orderList.add(newOrder);
+
+			}
+		} catch (InvalidOrderTimeStamp invalidTimestamp) {
+			System.err.print("Wrong format of timestamp!");
+			while (sc2.hasNext())
+				sc2.nextLine();
+		} catch (InvalidOrderCustomerID invalidCustomer) {
+			System.err.print("Wrong format of customer!");
+			while (sc2.hasNext())
+				sc2.nextLine();
 		}
 		sc2.close();
 		return orderList;
 
 	}
 
+	public Item findItem(String itemID) {
 
-	public TreeSet <Item> readMenuInfo(String filename) throws FileNotFoundException{
-		TreeSet <Item> itemList = new TreeSet <Item> (new ItemComparator());
-		File file1 = new File(filename);
-		Scanner sc1 = new Scanner(file1);
-		// we don't need a Map since we don't have a key for the menu, we don't need a set since we don't want to get rid of the duplicates
-		String line = sc1.nextLine();
-		while (sc1.hasNextLine()) {
-			line = sc1.nextLine();
-			String[] values = line.split(",");
-			String name = values[0];
-			String description = values[1];
-			double price = Double.parseDouble(values[2]);
-			String itemID = values[3];
-			String category = values[4];
-
-
-			Item menuItem = new Item(name, description, price, itemID, category);
-			itemList.add(menuItem);
-		}	
-		sc1.close();
-		return itemList;	
+		for (Item item : itemList) {
+			if (item.getItemID().equals(itemID)) {
+				return item;
+			}
+		}
+		return null;
 	}
-}
 
+}
--- OrderShop/src/shop/Order.java
+++ OrderShop/src/shop/Order.java
@@ -2,62 +2,63 @@ package shop;
 
 import java.util.HashMap;
 
+// This is the Order class that handles the features of each order
 public class Order {
 	private String TimeStamp;
 	private String CustomerID;
-	private HashMap<String,Integer> Items;
-	
-	
-	public Order(String TimeStamp, String CustomerID){
-		
+	private String CustomerName;
+	private HashMap<String, Integer> Items;
+	private double price;
+
+	public Order(String TimeStamp, String CustomerID, String CustomerName) {
+
 		this.TimeStamp = TimeStamp;
 		this.CustomerID = CustomerID;
-		this.Items = new HashMap<String,Integer>();
-	}
-	
-	public Order(String TimeStamp, String CustomerID, String itemID){
-		this.Items = new HashMap<String,Integer>();
-		this.TimeStamp = TimeStamp;
-		this.CustomerID = CustomerID;
-		this.Items.put(itemID, 1);
-		
+		this.CustomerName = CustomerName;
+		this.Items = new HashMap<String, Integer>();
 	}
 
+	public Order(String TimeStamp, String CustomerID, String CustomerName, Item item) {
+		this.TimeStamp = TimeStamp;
+		this.CustomerID = CustomerID;
+		this.CustomerName = CustomerName;
+		this.Items = new HashMap<String, Integer>();
+		this.Items.put(item.getItemID(), 1);
+
+	}
+	
 	public String getTimeStamp() {
 		return TimeStamp;
 	}
-	
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double p) {
+		this.price = p;
+	}
+
 	public String getItemDescription(Item item) {
 		return item.getDescription();
 	}
-	
-	public void setTimeStamp(String TimeStamp) {
-		this.TimeStamp = TimeStamp;
-	}
-	
-	public void setCustomerID(String CustomerID) {
-		this.CustomerID = CustomerID;
-	}
-	
+
 	public String getCustomerID() {
 		return this.CustomerID;
 	}
+	
+	public String getCustomerName() {
+		return this.CustomerName;
+	}
 
-	public void addItem (Item item, int quantity) {
-		if(this.Items.containsKey(item.getItemID()))
+	public void addItem(Item item, int quantity) {
+		if (this.Items.containsKey(item.getItemID()))
 			this.Items.put(item.getItemID(), this.Items.get(item.getItemID()) + quantity);
 		else
 			this.Items.put(item.getItemID(), quantity);
 	}
-	public void addItem (String itemID, int quantity) {
-		if(this.Items.containsKey(itemID))
-			this.Items.put(itemID, this.Items.get(itemID) + quantity);
-		else
-			this.Items.put(itemID, quantity);
-	}
-	
-	public HashMap<String,Integer> getItems() {
+
+	public HashMap<String, Integer> getItems() {
 		return Items;
 	}
 }
-
